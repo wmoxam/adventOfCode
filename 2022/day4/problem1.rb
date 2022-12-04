a4678cce2b24be3aa38214c04a46aed207e946c0
wmoxam <me@wmoxam.com>
2022-12-04 00:12:34 -0500

    Day 4

---
+++ 2022/day4/problem1.rb
@@ -0,0 +1,17 @@
+score = 0
+
+$stdin.read.each_line do |s|
+	a,b = s.split(/,/)
+	a1,a2 = a.split(/-/).map(&:to_i)
+	b1,b2 = b.split(/-/).map(&:to_i)
+	range1 = a1..a2
+	range2 = b1..b2
+
+	if range1.to_a.all? {|c| range2.include?(c) }
+		score += 1 
+	elsif range2.to_a.all? {|c| range1.include?(c) }
+		score += 1
+	end 
+end
+
+puts score
