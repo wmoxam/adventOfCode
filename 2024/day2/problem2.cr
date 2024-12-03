5e676524b45be8cc532cd67d0a49252b9ab72fae
“Wesley <wes@crowdmark.com>
2024-12-02 19:40:14 -0500

    Add Crystal version

---
+++ 2024/day2/problem2.cr
@@ -0,0 +1,26 @@
+safe = 0
+ARGF.gets_to_end.each_line do |report|
+  all_digits = report.split(/\s+/).map {|d| d.to_i }
+
+  all_digits.combinations(all_digits.size - 1).each do |digits|
+    next unless digits == digits.sort || digits == digits.sort.reverse
+
+    ok = true
+    last = digits.first
+    digits[1..-1].each do |d|
+      abs = (last - d).abs
+      if abs < 1 || abs > 3
+        ok = false
+        break
+      end
+      last = d
+    end
+
+    next unless ok
+
+    safe += 1
+    break
+  end
+end
+
+puts safe
